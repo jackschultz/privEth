dc6196ac764f18972f986a1ceccca76cc649a70d
Jack Schultz <jackschultz23@gmail.com>
2017-12-15 08:26:03 -0600

    updating the contract in the repo

--- contracts/Questions.sol
+++ contracts/Questions.sol
@@ -1,25 +1,41 @@
 pragma solidity ^0.4.0;
 contract Questions {
 
-    struct Question {
-        address asker;
-        string wording;
-    }
-    
-    Question question;
-    mapping(address => bool) public answers;
+  mapping(address => uint) public answers;
+  string question;
+  address asker;
+  uint trues;
+  uint falses;
 
-    /// __init__
-    function Questions(string _wording) public {
-        question.asker = msg.sender;
-        question.wording = _wording;
-    }
+  /// __init__
+  function Questions(string _question) public {
+    asker = msg.sender;
+    question = _question;
+  }
 
-    function answerQuestion (bool _answer) public {
-        answers[msg.sender] = _answer;
+  function answerQuestion (bool _answer) public {
+    if (answers[msg.sender] == 0 && _answer) { //haven't answered yet
+      answers[msg.sender] = 1; //they vote true
+      trues += 1;
     }
+    else if (answers[msg.sender] == 0 && !_answer) {
+      answers[msg.sender] = 2; //falsity
+      falses += 1;
+    }
+    else if (answers[msg.sender] == 2 && _answer) { // false switching to true
+      answers[msg.sender] = 1; //true
+      trues += 1;
+      falses -= 1;
+    }
+    else if (answers[msg.sender] == 1 && !_answer) { // true switching to false
+      answers[msg.sender] = 2; //falsity
+      trues -= 1;
+      falses += 1;
+    }
+  }
 
-    function getQuestion () {
-    }
+  function getQuestion() public constant returns (string, uint, uint, uint) {
+    return (question, trues, falses, answers[msg.sender]);
+  }
 
 }
